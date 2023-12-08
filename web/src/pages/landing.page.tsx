6082110d2913192b80ba7cf92807c013c24ffa6e
kvarela <karim@metalink.com>
2023-12-07 20:11:08 -0800

    Correct QR code

--- web/src/pages/landing.page.tsx
+++ web/src/pages/landing.page.tsx
@@ -6,8 +6,9 @@ import QRCode from 'qrcode.react'
 function LandingPage() {
   return (
     <div>
-      <h1>Welcome to Our Gym</h1>
-      <QRCode value="http://example.com/check-in" size={256} />
+      <h1>Welcome to Uprise</h1>
+      <h2>Check in by scanning the QR code below</h2>
+      <QRCode value={`${process.env.REACT_APP_WEB_URL}/check-in`} size={256} />
       <div>
         <Link to="/login">Login</Link>
         <Link to="/register">Register</Link>
