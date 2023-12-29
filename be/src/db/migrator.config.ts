416d1b7a1f65ed41350433ae42a21a06f38770fa
kvarela <karim@metalink.com>
2023-12-29 15:47:41 -0800

    moved typeorm config into migrator.config

--- be/src/db/migrator.config.ts
+++ be/src/db/migrator.config.ts
@@ -1,7 +1,18 @@
-import { DataSource, DataSourceOptions } from 'typeorm'
-import { TYPEORM_OPTIONS } from '../typeorm.options'
+import { DataSource } from 'typeorm'
+import { join } from 'path'
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+console.log('process.env.DATABASE_URL', process.env.DATABASE_URL)
+console.log('__dirname', __dirname)
 
 export default new DataSource({
-  ...(TYPEORM_OPTIONS as DataSourceOptions),
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  synchronize: false,
+  entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+  namingStrategy: new SnakeNamingStrategy(),
   migrations: ['./src/db/migrations/*.ts']
 })
