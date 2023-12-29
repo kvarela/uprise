aac923773d5251c1f31fc503bf7e6aba5ae08944
kvarela <karim@metalink.com>
2023-12-29 15:51:11 -0800

    Explicit entity names

--- be/src/db/migrator.config.ts
+++ be/src/db/migrator.config.ts
@@ -1,18 +1,21 @@
 import { DataSource } from 'typeorm'
-import { join } from 'path'
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
 import * as dotenv from 'dotenv'
+import { CheckIn } from '../check-in/check-in.entity'
+import { Class } from '../class/class.entity'
+import { Member } from '../member/member.entity'
+import { MembershipType } from '../member/membership-type.entity'
+import { Style } from '../style/style.entity'
 
 dotenv.config()
 
 console.log('process.env.DATABASE_URL', process.env.DATABASE_URL)
-console.log('__dirname', __dirname)
 
 export default new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
   synchronize: false,
-  entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+  entities: [CheckIn, Class, Member, MembershipType, Style],
   namingStrategy: new SnakeNamingStrategy(),
   migrations: ['./src/db/migrations/*.ts']
 })
